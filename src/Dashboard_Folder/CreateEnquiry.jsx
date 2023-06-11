8a4ff16d9cf9de0c388d504c699d7184445997be
irshad ali <irshad.ali052@gmail.com>
2023-06-12 00:02:19 +0530

    Dashboard main page is commpleted

---
+++ src/Dashboard_Folder/CreateEnquiry.jsx
@@ -0,0 +1,12 @@
+import '../navbar_use.css'
+import '../Container.css'
+function CreateEnquiry(){
+    return(
+        <div className="container-fluid home-container">
+            <div className='home-div1-content'>
+                  <center><h1>Create Enquiry</h1></center>
+            </div>
+        </div>
+    )
+}
+export default CreateEnquiry;
